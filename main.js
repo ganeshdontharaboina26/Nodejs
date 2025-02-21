c70e9c550c9b6e53c11f247d471c9af729c93f83
ganeshdontharaboina26 <ganeshdontharaboina26@gmail.com>
2025-02-21 11:24:46 +0530

    new server

--- main.js
+++ main.js
@@ -23,15 +23,24 @@
 //         })
 //     }
 // })
-console.log("sdfghkjl")
-console.log("hhbjnkm")
-console.log("fgvhbjnm")
+// console.log("sdfghkjl")
+// console.log("hhbjnkm")
+// console.log("fgvhbjnm")
 
+// const http=require("http")
+// const server=http.createServer((req,res)=>{
+//     res.write("hello ganesh")
+//     res.end("creating a server.....")
+// })
+// server.listen("3101",()=>{
+//     console.log("server is running.......")
+// })
 const http=require("http")
-const server=http.createServer((req,res)=>{
-    res.write("hello ganesh")
-    res.end("creating a server.....")
-})
-server.listen("3101",()=>{
-    console.log("server is running.......")
+const server=http.createServer((req,res) => {
+    res.write("thank you ")
+    res.end("sathish")
+}
+);
+server.listen("4004",()=>{
+    console.log("sever is running")
 })
